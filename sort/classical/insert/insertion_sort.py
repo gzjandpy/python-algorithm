96c0fe1bf700ee4eac10c415a9d466bf7ab2ff68
gzj <864060674@qq.com>
2019-07-21 18:58:55 +0800

    fix insertion_sort

--- sort/classical/insert/insertion_sort.py
+++ sort/classical/insert/insertion_sort.py
@@ -9,13 +9,13 @@ def insertion_sort(arr):
     """
     n = len(arr)
     for i in range(1, n):
-        cursor = arr[i]
+        element = arr[i]
         pos = i
 
-        while pos > 0 and arr[pos - 1] > cursor:
+        while pos > 0 and arr[pos - 1] > element:
             arr[pos] = arr[pos - 1]
             pos -= 1
-        arr[pos] = cursor
+        arr[pos] = element
     return arr
 
 
